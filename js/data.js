6cb7ef3566a007ed9e51d0c8b52cd970640f6585
김예준 <kimyejun978@gmail.com>
2026-06-01 08:59:16 +0900

    modify data.js's bug(1)

--- js/data.js
+++ js/data.js
@@ -2,10 +2,10 @@ const scd = document.querySelector("#schedule_malloc");
 
 let row = 3;
 
-for (let i = 0; i <= row; i++) {
+for (let i = 1; i <= row; i++) {
     const tr = document.createElement("tr");
     tr.innerHTML = `
-    <td>${1}교시</td>
+    <td>${i}교시</td>
     <td>변경 전 과목</td>
     <td>변경 후 과목</td>
     `;
