fa3a6f92403fc874e19173eb8f9f4f3a78ac4411
kimyejun978-tech <kimyejun978@gmail.com>
2026-09-11 11:35:48 +0900

    feat: 오늘체육 런타임 설정 파일 추가

---
+++ today-pe/config.js
@@ -0,0 +1,8 @@
+// 오늘체육 런타임 설정
+// Supabase를 연결하려면 프로젝트 URL과 anon/publishable key를 입력하세요.
+// anon/publishable key는 브라우저 공개용 키이며, service_role 키는 절대 넣지 마세요.
+window.ONEUL_PE_CONFIG = {
+  SUPABASE_URL: '',
+  SUPABASE_ANON_KEY: '',
+  NEIS_API_KEY: '',
+};
