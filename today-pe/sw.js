0209593fe582fe41bd96ff492d73a6ba5c8b2b63
kimyejun978-tech <kimyejun978@gmail.com>
2026-09-15 18:47:51 +0900

    style: 새 디자인 CSS를 PWA 캐시에 반영

--- today-pe/sw.js
+++ today-pe/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'oneul-pe-v12';
+const CACHE_NAME = 'oneul-pe-v13';
 const STATIC_ASSETS = [
   './',
   './index.html',
@@ -8,6 +8,7 @@ const STATIC_ASSETS = [
   './style.css',
   './student-extra.css',
   './teacher-extra.css',
+  './design-refresh.css',
   './shared.js',
   './backend.js',
   './student-privacy.js',
